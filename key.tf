47264293092da2ad870b5b57f166ccfe12041b86
mariamiah <mariah.kakande@gmail.com>
2019-04-08 12:32:19 +0300

    Update key.tf

--- key.tf
+++ key.tf
@@ -1,3 +1,4 @@
+# This resource defines the Key pair that shall be used to access AWS
 resource "aws_key_pair" "mykeypair" {
   key_name   = "myAwsKey"
   public_key = "${file("${var.PUBLIC_KEY}")}"
